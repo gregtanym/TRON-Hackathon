89df26bbe259eb8f2acb888f1d572498392680ad
gregtanym <gregtanym@gmail.com>
2024-04-11 19:08:50 +0800

    Create HomeHero Component for Hompage

--- app/page.jsx
+++ app/page.jsx
@@ -2,17 +2,17 @@
 import React from 'react'
 import { useGlobalContext } from './Context/store'
 import Nav from '@/components/Nav'
-import HomeHero from '@/components/HomeHero'
-import CategoryFeed from '@/components/CategoryFeed'
-import ConcertFeed from '@/components/ConcertFeed'
+import HomeHero from '@/components/homepage/HomeHero'
+import CategoryFeed from '@/components/homepage/CategoryFeed'
+import ConcertFeed from '@/components/homepage/ConcertFeed'
 
 const Home = () => {
   const {testContextValue} = useGlobalContext()
   return (
-    <section className="w-full flex-center flex-col">
-        <HomeHero/>
-        <CategoryFeed/>
-        <ConcertFeed/>
+    <section className="w-full flex-center flex-col h-ful">
+      <HomeHero/>
+      <CategoryFeed/>
+      <ConcertFeed/>
     </section>
   )
 }
