0ec09d89accd2b53d995aa9d79b01aca637f0962
gregtanym <gregtanym@gmail.com>
2024-04-24 16:02:45 +0800

    Update myTickets page to work with multiple contract instances

--- app/Context/store.jsx
+++ app/Context/store.jsx
@@ -12,6 +12,7 @@ const AppProvider = (({children}) => {
     const [account, setAccount] = useState(''); // stores the current account connected
     const [network, setNetwork] = useState({});
     const adapter = useMemo(() => new TronLinkAdapter(), []);
+    const [myTickets, setMyTickets] = useState([])
 
     const [isLoading, setIsLoading] = useState(false)
 
@@ -27,21 +28,14 @@ const AppProvider = (({children}) => {
     // READ FUNCTIONS (NFT CONTRACT)
 
     const getOwnedTokenIds = async (ownerAddress, contractAddress) => {
-        // eventually i need the contract address to determine which contract im looking for information
-        // right now i shall just hardcode it to taylor contract only to make sure everything works first
-        const contract = await tronWeb.contract().at(process.env.NEXT_PUBLIC_TAYLOR_CONTRACT_ADDRESS)
+        const contract = await tronWeb.contract().at(contractAddress)
         const ownedTokens = await contract.getOwnedTokenIds(ownerAddress).call()
         console.log("this is owned tokens: ", ownedTokens)
         return ownedTokens
     }
 
-    const isEventCanceled = async (contractAddress) => {
-        const contract = await tronWeb.contract().at(process.env.NEXT_PUBLIC_TAYLOR_CONTRACT_ADDRESS)
-        return await contract.eventCanceled().call()
-    }
-
-    const getCatPrices = async (categoryId) => {
-        const contract = await tronWeb.contract().at(process.env.NEXT_PUBLIC_TAYLOR_CONTRACT_ADDRESS)
+    const getCatPrices = async (categoryId, contractAddress) => {
+        const contract = await tronWeb.contract().at(contractAddress)
         const ticketPrice = await contract.categoryPrices(categoryId).call()
         const decimalPrice = tronWeb.toDecimal(ticketPrice._hex)
         
@@ -50,8 +44,8 @@ const AppProvider = (({children}) => {
     }
 
     // const getMintLimit = (contractAddress) => {
-    const getMintLimit = async () => {
-        const contract = await tronWeb.contract().at(process.env.NEXT_PUBLIC_TAYLOR_CONTRACT_ADDRESS)
+    const getMintLimit = async (contractAddress) => {
+        const contract = await tronWeb.contract().at(contractAddress)
         const mintLimit = await contract.mintLimitPerAddress().call()
         const decimalLimit = tronWeb.toDecimal(mintLimit._hex)
         return decimalLimit
@@ -60,33 +54,95 @@ const AppProvider = (({children}) => {
     // function to get all the owned tokens across all event contracts
     const getAllOwnedTokens = async (userAddress) => {
         try {
-            console.log("getAllOwnedTokens called: ", userAddress)
-            let allOwnedTokens = {}
+            console.log("getAllOwnedTokens called: ", userAddress);
+            let allNewTickets = []; // Aggregate all tickets here
+    
+            // Wait for all promises from map to resolve
             await Promise.all(eventData.map(async (event) => {
-                console.log(event.eventTitle, event.contractAddress)
-                const contract = await tronWeb.contract().at(event.contractAddress)
-                const ownedTokenIds = await contract.getOwnedTokenIds(userAddress).call()
-                console.log(event.eventTitle, "tickets found: ", await ownedTokenIds)
-
-                // convert the hex to decimal
-                allOwnedTokens[event.contractAddress] = ownedTokenIds.map(tokenId =>
-                    tronWeb.toDecimal(tokenId._hex)
-                );
-            }))
-            return allOwnedTokens
+                console.log(event.eventTitle, event.contractAddress);
+                const currentContractAddress = event.contractAddress;
+                const contract = await tronWeb.contract().at(currentContractAddress);
+                const ownedTokenIds = await contract.getOwnedTokenIds(userAddress).call();
+                console.log(event.eventTitle, "tickets found: ", ownedTokenIds);
+    
+                // Temporary array for this contract
+                let tempTickets = [];
+                for (let i = 0; i < ownedTokenIds.length; i++) {
+                    const currentTokenId = tronWeb.toDecimal(ownedTokenIds[i]._hex);
+                    const isRedeemed = await contract.isTicketRedeemed(currentTokenId).call();
+                    const isInsured = await contract.ticketInsurance(currentTokenId).call();
+                    const catIndex = await contract.determineCategoryId(currentTokenId).call();
+                    const catClass = tronWeb.toDecimal(catIndex) + 1;
+                    const imageURL = await contract.tokenURI(currentTokenId).call();
+                    const isCancelled = await contract.eventCanceled().call();
+    
+                    const newTicket = {
+                        "contractAddress": currentContractAddress, 
+                        "eventId": event.eventId,
+                        "eventTitle": event.eventTitle,
+                        "date": event.date,
+                        "time": event.time, 
+                        "location": event.location, 
+                        "tokenId": currentTokenId,
+                        "isRedeemed": isRedeemed,
+                        "isInsured": isInsured,
+                        "catClass": catClass,
+                        "imageURL": imageURL,
+                        "isCancelled": isCancelled,
+                        "originalTicketPrice": tronWeb.toSun(event.catPricing[catIndex])
+                    };
+                    // i need a isListed checker
+    
+                    tempTickets.push(newTicket);
+                }
+    
+                // Combine the tickets from this iteration into the main array
+                allNewTickets = allNewTickets.concat(tempTickets);
+            }));
+    
+            // Now update the state once with all new tickets
+            setMyTickets(allNewTickets);
         } catch (error) {
             console.error("Error in getAllOwnedTokens: ", error);
-            throw error; // Or return a default value or error code as needed
+            throw error;
         }
-        
+    }
+
+    const isTicketRedeemed = async (contractAddress, tokenId) => {
+        const contract = await tronWeb.contract().at(contractAddress)
+        const isRedeemed = await contract.isTicketRedeemed(tokenId).call()
+        return isRedeemed
+    }
+
+    const isTicketInsured = async (contractAddress, tokenId) => {
+        const contract = await tronWeb.contract().at(contractAddress)
+        const isInsured = await contract.ticketInsurance(tokenId).call()
+        return isInsured
+    }
+
+    const getCategory = async (contractAddress, tokenId) => {
+        const contract = await tronWeb.contract().at(contractAddress)
+        const catIndex = await contract.determineCategoryId(tokenId).call()
+        const catClass = catIndex + 1
+        return catClass
+    }
+
+    const getTokenURI = async (contractAddress, tokenId) => {
+        const contract = await tronWeb.contract().at(contractAddress)
+        const imageURL = await contract.tokenURI(tokenId).call()
+        return imageURL
+    }
+
+    const isEventCanceled = async (contractAddress) => {
+        const contract = await tronWeb.contract().at(contractAddress)
+        return await contract.eventCanceled().call()
     }
 
     // WRITE FUNCTIONS (NFT CONTRACT)
 
-    const mintTicket = async (categoryId, quantity, fee) => {
-        console.log(categoryId, quantity, fee)
+    const mintTicket = async (categoryId, quantity, fee, contractAddress) => {
         try {
-            const contract = await tronWeb.contract().at(process.env.NEXT_PUBLIC_TAYLOR_CONTRACT_ADDRESS)
+            const contract = await tronWeb.contract().at(contractAddress)
             const result = await contract.mintTicket(categoryId, quantity).send({
                 feeLimit: 1000000000,
                 callValue: fee * quantity,
@@ -98,7 +154,39 @@ const AppProvider = (({children}) => {
             console.log("Error minting ticket: ", error)
             return { success: false, error }
         }
-        
+    }
+
+    const buyInsurance = async (contractAddress, tokenId, originalTicketPrice) => {
+        try {
+            const contract = await tronWeb.contract().at(contractAddress)
+            const insurancePrice = parseInt(originalTicketPrice) * 20/100
+            const result = await contract.buyInsurance(tokenId).send({
+                feeLimit: 1000000000,
+                callValue: insurancePrice,
+                shouldPollResponse: true
+            })
+            console.log("buy ticket insurance: ", result)
+            return {success: true, result}
+        } catch (error) {
+            console.log("Error buying ticket insurance: ", error)
+            return { success: false, error }
+        }
+    }
+
+    const redeemTicket = async (contractAddress, tokenId) => {
+        try {
+            const contract = await tronWeb.contract().at(contractAddress)
+            const result = await contract.redeemTicket(tokenId).send({
+                feeLimit: 1000000000,
+                callValue: 0,
+                shouldPollResponse: true
+            })
+            console.log("redeem ticket: ", result)
+            return {success: true, result}
+        } catch (error) {
+            console.log("Error redeeming ticket: ", error)
+            return { success: false, error }
+        }
     }
 
     // UTILITY FUNCTIONS
@@ -123,10 +211,13 @@ const AppProvider = (({children}) => {
 
     return(
         <AppContext.Provider value={{
-            adapter, readyState, account, network,isLoading,
-            setReadyState, setAccount, setNetwork,setIsLoading,
+            adapter, readyState, account, network, isLoading, myTickets,
+            setReadyState, setAccount, setNetwork, setIsLoading, setMyTickets,
             tronWeb, 
-            getOwnedTokenIds, mintTicket, isTronLinkConnected, getCatPrices, decodeHexString, getMintLimit, getAllOwnedTokens
+            isTronLinkConnected,
+            getOwnedTokenIds, getCatPrices, getMintLimit, getAllOwnedTokens,
+            mintTicket, buyInsurance, redeemTicket,
+            decodeHexString,
         }}>
             {children}
         </AppContext.Provider>
--- app/mytickets/page.jsx
+++ app/mytickets/page.jsx
@@ -2,21 +2,41 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import { useGlobalContext } from '../Context/store'
+import TicketCard from '@/components/mytickets-page/TicketCard'
+import Loading from '@/components/Loading'
 
 const MyTickets = () => {
 
-  const {account, getAllOwnedTokens} = useGlobalContext()
-  const [myTickets, setMyTickets] = useState([])
+  const {account, getAllOwnedTokens, myTickets, isLoading} = useGlobalContext()
+
+  const [selectedFilter, setSelectedFilter] = useState("Purchased")
+
+  const handleFilterChange = (event) => {
+    setSelectedFilter(event.target.value);
+  };
+
+  const filteredTickets = myTickets.filter(ticket => {
+    switch(selectedFilter) {
+      case 'Purchased':
+        return true;
+      case 'Listed':
+        return false; // Update when listed is implemented
+      case 'Redeemed':
+        return ticket.isRedeemed;
+      case 'Insured':
+        return ticket.isInsured;
+      default:
+        return true;
+    }
+  });
 
   useEffect(() => {
     const fetchTickets = async () => {
-      if (account) { // Ensure account is not undefined or null
+      if (account) { 
         try {
-          const tickets = await getAllOwnedTokens(account);
-          setMyTickets(tickets); // Set tickets to state after they are fetched
+          await getAllOwnedTokens(account);
         } catch (error) {
           console.error("Failed to fetch tickets:", error);
-          setMyTickets([]); // Handle errors, maybe set to an empty array or show error message
         }
       }
     };
@@ -24,31 +44,49 @@ const MyTickets = () => {
   }, [account])
 
   useEffect(() => {
-    console.log("this is myTickets ", myTickets);
-  }, [myTickets]);
+    console.log(myTickets)
+  }, [myTickets])
 
   return (
     <section className="w-full flex flex-col items-center">
+      {isLoading && <Loading/>}
       <div className=' flex self-start mt-8 px-12'>
         <div className='flex flex-col items-center'>
           <h2 className="text-3xl font-bold">MY TICKETS</h2>
           <div className="w-36 border-b-2 border-black mt-2"></div>
         </div>
       </div>
-          <div className='self-end mx-12 mt-1'>
-            <select name="location" id="location" className="outline-none rounded-md py-2 px-4 bg-gray-800 text-white font-semibold">
-              <option>Purchased</option>
-              <option>Listed</option>
-              <option>Redeemed</option>
-              <option>Insured</option>
-            </select>
-          </div>
-          <div>
-            {!account && "Please connect your TronLink Wallet to see your NFTickets"}
-            {account}
-            i need to get the concert info and also the individual tickt information (isredeemed, isinsured, islisted)
-            and also need to know if the event is cancelled from the nft contract
-          </div>
+      <div className='self-end mx-12 mt-1'>
+        <select name="location" id="location" className="outline-none rounded-md py-2 px-4 bg-gray-800 text-white font-semibold" onChange={handleFilterChange}>
+          <option>Purchased</option>
+          <option>Listed</option>
+          <option>Redeemed</option>
+          <option>Insured</option>
+        </select>
+      </div>
+      <div className='w-3/4 mb-10'>
+        {!account && "Please connect your TronLink Wallet to see your NFTickets"}
+        <br/>
+        <div className='w-full flex justify-center'>
+          {filteredTickets.length === 0 && "No tickets to display :("}
+        </div>
+        {filteredTickets.map(event => <TicketCard
+          key={event.eventId}
+          contractAddress= {event.contractAddress}
+          eventId={event.eventId}
+          title={event.eventTitle}
+          date={event.date}
+          time={event.time}
+          location={event.location}
+          tokenId={event.tokenId}
+          isRedeemed={event.isRedeemed}
+          isInsured={event.isInsured}
+          catClass={event.catClass}
+          imageURL={event.imageURL}
+          isCancelled={event.isCancelled}
+          originalTicketPrice = {event.originalTicketPrice}
+        />)}
+      </div>
       </section>
   )
 }
